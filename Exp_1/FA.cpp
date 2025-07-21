59acd609f967e32e52d13d02e1763bfa88048881
rohan3154 <rohan31054@gmail.com>
2025-07-21 06:56:13 +0000

    exp_1 half done

---
+++ Exp_1/FA.cpp
@@ -0,0 +1,19 @@
+// Sample Program-1: Create a program in Python that implements a Finite Automata to validate strings that start with 'a' and end with 'b'.
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+bool Finite_automata(string s) {
+    int n = s.length();
+    string start_state = "q0";
+    string start_state = "q3";
+    string current_state = "q0";
+
+    
+}
+
+int main() {
+    return 0;
+}
